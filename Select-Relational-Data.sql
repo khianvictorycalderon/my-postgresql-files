c2d97ecca885981c67af6976e64200fcfcef4828
khian calderon <ckhianvictory@gmail.com>
2025-11-09 21:25:19 +0800

    Added column mixing selection

--- Select-Relational-Data.sql
+++ Select-Relational-Data.sql
@@ -7,3 +7,12 @@ SELECT
 
     FROM customer_order
     JOIN customer ON customer_order.customer_id = customer.customer_id;
+
+--- This combines first name and last name into one column
+SELECT
+    customer_order.customer_order_id,
+    customer_order.customer_order_name,
+    customer.customer_last_name || ', ' || customer.customer_first_name as customer_full_name
+
+    FROM customer_order
+    JOIN customer ON customer_order.customer_id = customer.customer_id;
