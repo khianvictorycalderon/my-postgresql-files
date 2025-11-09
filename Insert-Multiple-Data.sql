647fa32a1eb21b4167d98f9c555d2e0942322494
khian calderon <ckhianvictory@gmail.com>
2025-11-09 20:52:55 +0800

    Added mutiple data insertion

---
+++ Insert-Multiple-Data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_email, user_name, user_age)
+    VALUES
+        ('sampleEmail1@fake.mail', 'Jena', 19),
+        ('sampleEmail2@fake.mail', 'John', 21),
+        ('sampleEmail3@fake.mail', 'Jake', 16),
+        ('sampleEmail4@fake.mail', 'Anton', 24),
+        ('sampleEmail5@fake.mail', 'Blake', 22);
