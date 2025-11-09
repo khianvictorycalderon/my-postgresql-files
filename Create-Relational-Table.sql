dc13927f0c6df53e19535a856e3cda3d15ca1c1c
khian calderon <ckhianvictory@gmail.com>
2025-11-09 21:11:19 +0800

    Added sample insert relational data

--- Create-Relational-Table.sql
+++ Create-Relational-Table.sql
@@ -11,3 +11,24 @@ CREATE TABLE IF NOT EXISTS customer_order (
     customer_id UUID NOT NULL REFERENCES customer(customer_id) ON DELETE CASCADE,
     customer_order_name TEXT NOT NULL
 );
+
+--- Example Insert Data
+
+-- Insert 5 customers
+INSERT INTO customer (customer_first_name, customer_last_name) VALUES
+('Khian', 'Victory'),
+('Alice', 'Wonderland'),
+('Bob', 'Builder'),
+('Carol', 'Danvers'),
+('Dave', 'Johnson');
+
+-- Insert 8 orders
+INSERT INTO customer_order (customer_id, customer_order_name) VALUES
+((SELECT customer_id FROM customer WHERE customer_first_name='Khian' AND customer_last_name='Victory'), 'Order A'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Khian' AND customer_last_name='Victory'), 'Order B'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Alice' AND customer_last_name='Wonderland'), 'Order C'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Bob' AND customer_last_name='Builder'), 'Order D'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Carol' AND customer_last_name='Danvers'), 'Order E'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Carol' AND customer_last_name='Danvers'), 'Order F'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Dave' AND customer_last_name='Johnson'), 'Order G'),
+((SELECT customer_id FROM customer WHERE customer_first_name='Dave' AND customer_last_name='Johnson'), 'Order H');
