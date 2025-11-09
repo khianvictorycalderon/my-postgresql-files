9547646484dbecaaeca9858498b438b01f47a04b
khian calderon <ckhianvictory@gmail.com>
2025-11-09 21:33:08 +0800

    Added if not exists

--- Create-Table-Insert-Data.sql
+++ Create-Table-Insert-Data.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSIONS IF NOT EXISTS "pgcrypto"
 
-CREATE TABLE people (
+CREATE TABLE IF NOT EXISTS people (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
