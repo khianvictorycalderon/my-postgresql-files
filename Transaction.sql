47ad45d8e4ddb9b548654b1e965ab3e4c7d85448
khian calderon <ckhianvictory@gmail.com>
2025-11-17 18:00:52 +0800

    Updated transaction: Added objective table

--- Transaction.sql
+++ Transaction.sql
@@ -1,5 +1,13 @@
-BEGIN;
-    --- Basically what this means is, all happens or none, not one or the other
+-- Sample Table only
+CREATE TABLE customers (
+    id SMALLSERIAL PRIMARY KEY,
+    customer_name VARCHAR(50) NOT NULL,
+    balance INT NOT NULL DEFAULT 0
+);
+
+-- Actual Transaction
+BEGIN TRANSACTION; -- or just BEGIN
+    -- Basically what this means is, all happens or none, not one or the other
     UPDATE customers SET balance = balance + 100 WHERE id = 1;
-    UPDATE Customers SET balance = balance - 100 WHERE id = 2;
-COMMIT;
+    UPDATE customers SET balance = balance - 100 WHERE id = 2;
+COMMIT TRANSACTION; -- or simply just COMMIT
