c1698cece92eb9d20351c61eada70b25f95f3ad5
khian calderon <ckhianvictory@gmail.com>
2025-11-17 19:00:44 +0800

    Fixed typo

--- Ceil-Function.sql
+++ Ceil-Function.sql
@@ -1,2 +1,2 @@
--- Returns the smalles integer value that is not smaller than X
+-- Returns the smallest integer value that is not smaller than X
 SELECT CEILING(4.46); -- Returns 5 because 5 is the neareast integer not less than 4.46
