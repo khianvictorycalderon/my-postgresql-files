f2c786757d47268286e58c82fe528e5f68524b69
khian calderon <ckhianvictory@gmail.com>
2025-11-17 19:12:13 +0800

    Added round function

---
+++ Round-Function.sql
@@ -0,0 +1,5 @@
+-- Basically rounds of the number to the neareast integer
+-- Takes 2 arguments X = Number to round, Y = how many decimal places and is optional
+SELECT ROUND(5.45); -- Returns 5
+SELECT ROUND(4.575); -- Also returns 5
+SELECT ROUND(8.386430, 2); -- Returns 8.39
