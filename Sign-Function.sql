0a1eb5dde4a51a0c78051eebc079c8cb21ebcdb8
khian calderon <ckhianvictory@gmail.com>
2025-11-17 19:13:39 +0800

    Added sign function

---
+++ Sign-Function.sql
@@ -0,0 +1,4 @@
+-- Returns either -1, 0, or 1, depending on the sign of X
+SELECT SIGN(4.46); -- Returns 1 because 4.46 is a positive number
+SELECT SIGN(0); -- Returns 0 well because it is 0
+SELECT SIGN(-3.23); -- Returns 1 because -3.23 is a negative number
