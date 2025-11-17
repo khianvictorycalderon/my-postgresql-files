c9a661675c6c66a243047ea0873c9d9df776c527
khian calderon <ckhianvictory@gmail.com>
2025-11-17 18:42:00 +0800

    Fixed max function missing semicolon

--- Max-Function.sql
+++ Max-Function.sql
@@ -1,4 +1,4 @@
 -- Returns the highest value among a record set
 SELECT customer_name, balance
     FROM customers
-    WHERE balance = (SELECT MAX(balance) FROM customers)
+    WHERE balance = (SELECT MAX(balance) FROM customers);
