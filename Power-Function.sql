9c9f3269a7af7a78f9b007ae555233394cb36e4c
khian calderon <ckhianvictory@gmail.com>
2025-11-17 19:19:24 +0800

    Updated power of function: Cube root

--- Power-Function.sql
+++ Power-Function.sql
@@ -1,2 +1,5 @@
 -- Returns the X to the power of Y, basically X multipying Y times to itself
 SELECT POW(5, 3); -- Basically means 5 times 5 times 5
+
+-- Bonus: For getting the cube root
+SELECT POWER(125, 1.0/3.0); -- Basically kind of reverse engineering (Must have decimal)
