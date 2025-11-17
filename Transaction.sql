5677218d8cefb340ef39214324169f600b362e05
khian calderon <ckhianvictory@gmail.com>
2025-11-17 18:07:15 +0800

    Updated transaction

--- Transaction.sql
+++ Transaction.sql
@@ -20,4 +20,4 @@ BEGIN TRANSACTION; -- or just BEGIN
     -- Basically what this means is, all happens or none, not one or the other
     UPDATE customers SET balance = balance + 100 WHERE id = 1;
     UPDATE customers SET balance = balance - 100 WHERE id = 2;
-COMMIT TRANSACTION; -- or simply just COMMIT
+COMMIT TRANSACTION; -- or simply just COMMIT, or ROLLBACK if something went wrong
