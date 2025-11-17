6795d22c887109dcf24701270d930e6fc1067716
khian calderon <ckhianvictory@gmail.com>
2025-11-17 18:39:46 +0800

    Update max functions

--- Max-Function.sql
+++ Max-Function.sql
@@ -1,2 +1,4 @@
 -- Returns the highest value among a record set
-SELECT MAX(balance) FROM customers;
+SELECT customer_name, balance
+    FROM customers
+    WHERE balance = (SELECT MAX(balance) FROM customers)
